0ddf96e6ffd914036b949c07056d8667bb8230cc
小武 <1376850803@qq.com>
2019-09-01 19:00:54 +0800

    主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-      home
-  </div>
+<!-- 最外层容器 -->
+ <el-container>
+     <!-- 左侧导航 -->
+   <el-aside>
+     左侧
+   </el-aside>
+    <!-- 右侧 -->
+    <el-container>
+       <!-- 头部 -->
+      <el-header>
+        头部
+      </el-header>
+        <!-- 主要区域 -->
+      <el-main>
+        主体
+      </el-main>
+    </el-container>
+ </el-container>
 </template>
 
 <script>
