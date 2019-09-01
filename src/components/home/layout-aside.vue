e92f6a692fad0e5f9272f9710003022781303dd0
小武 <1376850803@qq.com>
2019-09-01 23:40:05 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -7,7 +7,8 @@
       background-color="#323745"
       text-color="#adafb5"
       style='width:201px'
-      active-text-color="#ffd04b">
+      active-text-color="#ffd04b"
+      router>
 
       <el-menu-item index="2">
         <i class="el-icon-s-home"></i>
@@ -22,10 +23,10 @@
                <span>内容管理</span>
            </template>
            <!-- el-submenu 是折叠的 => el-menu-item  => 下拉内容时匿名插槽 ,显示内容时具名插槽 -->
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论管理</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index="/home/public">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">内容列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论管理</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
       </el-submenu>
       <el-submenu index=2>
             <!-- el-submenu定义具名插槽 title -->
@@ -33,12 +34,12 @@
               <i class="el-icon-s-custom"></i>
               <span>粉丝管理</span>
           </template>
-        <el-menu-item>图文数据</el-menu-item>
-        <el-menu-item>粉丝概况</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
+        <el-menu-item index='/home/fansdata'>图文数据</el-menu-item>
+        <el-menu-item index='/home/fansinfo'>粉丝概况</el-menu-item>
+        <el-menu-item index='/home/fanslist'>粉丝列表</el-menu-item>
+        <el-menu-item index='/home/fansimg'>粉丝画像</el-menu-item>
       </el-submenu>
-      <el-menu-item index="3">
+      <el-menu-item index="/home/account">
           <i class="el-icon-s-tools"></i>
           <span slot="title">账户管理</span>
       </el-menu-item>
