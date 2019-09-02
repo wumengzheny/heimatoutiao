17de2ca96f3177e330c121e9fcec44b102ddc1d5
小武 <1376850803@qq.com>
2019-09-02 10:48:54 +0800

    主页模块-默认导航

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/home'
 import Login from './views/login'
+import Main from './views/home/main.vue'
 
 Vue.use(Router)
 
@@ -9,7 +10,11 @@ export default new Router({
   routes: [{
     path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [{
+      path: '',
+      component: Main
+    }]
   },
   {
     path: '/',
