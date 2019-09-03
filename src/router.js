ca167e1679478be32b996c78a5002e1d79b127ca
小武 <1376850803@qq.com>
2019-09-04 00:07:49 +0800

    素材列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -17,6 +17,9 @@ export default new Router({
     }, {
       path: 'comment',
       component: () => import('./views/comment')
+    }, {
+      path: 'material',
+      component: () => import('./views/material')
     }]
   },
   {
