b788cab5fde9bf813576ed4f5f2a693553cf868d
小武 <1376850803@qq.com>
2019-09-03 20:11:11 +0800

    评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,13 +1,32 @@
 <template>
   <div>
-      评论列表
+    <el-card>
+      <!-- 插槽内容=>标题 -->
+      <bread-crumb slot="header">
+        <template slot='title'>
+          评论列表
+        </template>
+      </bread-crumb>
+      <el-table :data="list">
+         <el-table-column label="标题" width='500' prop='name'></el-table-column>
+        <el-table-column label="评论状态" prop="name"></el-table-column>
+        <el-table-column label="总评论数" prop="name"></el-table-column>
+        <el-table-column label="粉丝评论数" prop='name'></el-table-column>
+      <el-table-column label="操作" prop='name'></el-table-column>
+      </el-table>
+
+    </el-card>
+
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data () {
+    return {
+      list: []
+    }
+  } }
 </script>
 
 <style>
