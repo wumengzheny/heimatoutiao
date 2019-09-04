24ad06558a2b6138bfdedaee67121fef50a97ca6
小武 <1376850803@qq.com>
2019-09-04 18:47:08 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,12 +3,34 @@
       <bread-crumb slot="header">
       <template slot="title">素材管理</template>
       </bread-crumb>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+    <el-tab-pane label="全部素材" name="all">
+        <!-- 全部素材的内容 -->
+    </el-tab-pane>
+    <el-tab-pane label="收藏图片" name="collect">
+<!-- 收藏素材的内容 -->
+        </el-tab-pane>
+
+  </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'all'
+    }
+  },
+  methods: {
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: { collect: false }// 全部的数据为fasle就是查看所有的图片
 
+      })
+    }
+  }
 }
 </script>
 
