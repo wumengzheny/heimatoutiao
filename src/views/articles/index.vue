db19e6650d907f93aa3a2d75125541b22328b7e2
小武 <1376850803@qq.com>
2019-09-04 22:55:06 +0800

    -内容列表-页面结构-搜索工具栏

---
+++ src/views/articles/index.vue
@@ -0,0 +1,42 @@
+<template>
+<el-card>
+ <bread-crumb slot="header">
+      <template slot="title">内容管理</template>
+    </bread-crumb>
+
+    <el-form style="margin-left:40px">
+        <el-form-item label="文章状态:">
+<!-- 文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部 -->
+        <el-radio-group>
+          <el-radio>全部</el-radio>
+              <el-radio>草稿</el-radio>
+              <el-radio>待审核</el-radio>
+              <el-radio>审核通过</el-radio>
+              <el-radio>审核失败</el-radio>
+    </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道列表:">
+            <el-select></el-select>
+        </el-form-item>
+        <el-form-item label="时间选择:">
+             <el-date-picker
+      v-model="value1"
+      type="daterange"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期">
+    </el-date-picker>
+        </el-form-item>
+    </el-form>
+</el-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
