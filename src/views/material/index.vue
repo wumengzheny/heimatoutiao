c407e5e11310c4bc7ea44f80eab8d8b6692ab3a0
小武 <1376850803@qq.com>
2019-09-04 20:24:36 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,35 +1,38 @@
 <template>
   <el-card class="material">
-      <bread-crumb slot="header">
+    <bread-crumb slot="header">
       <template slot="title">素材管理</template>
-      </bread-crumb>
-      <el-tabs v-model="activeName" @tab-click="changeTab">
-    <el-tab-pane label="全部素材" name="all">
+    </bread-crumb>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
         <!-- 全部素材的内容 -->
-  <div class="card-list">
-      <el-card v-for="item in list" :key="item.id" class="img-card">
-          <img :src="item.url" alt="">
-          <el-row type="flex"  align="middle" justify="space-around" class="operate">
-            <i :style="{color:item.is_collected ? 'red':''}" class="el-icon-star-on"></i>
-            <i class="el-icon-delete-solid"></i>
+        <div class="card-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+            <el-row type="flex" align="middle" justify="space-around" class="operate">
+              <i :style="{color:item.is_collected ? 'red':''}" class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+
+         <!-- 分页 -->
+          <el-row type="flex" justify="center" class="fen">
+            <el-pagination @current-change="changePage" :current-page="page.page" :page-size="page.pageSize" background layout="prev, pager, next" :total="page.total"></el-pagination>
           </el-row>
-        </el-card>
-
-  </div>
-
-    </el-tab-pane>
-    <el-tab-pane label="收藏图片" name="collect">
-<!-- 收藏素材的内容 -->
-<div class="card-list">
-      <el-card v-for="item in list" :key="item.id" class="img-card">
-          <img :src="item.url" alt="">
-
-        </el-card>
-
-  </div>
-        </el-tab-pane>
-
-  </el-tabs>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
+        <!-- 收藏素材的内容 -->
+        <div class="card-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      <el-row type='flex' justify="center">
+          <el-pagination @current-change="changePage" :current-page="page.page" :page-size="page.pageSize"  :total="page.total" background layout="prev, pager, next" ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -38,63 +41,78 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        page: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.page = newPage
+      this.getMaterial()
+    },
+    // 切换页签
     changeTab () {
       // this.activeName是最新的页签
       // 加载不同类型的数据
       // all => 所有的数据
       // collect => 去加载收藏数据
+      this.page.page = 1
       this.getMaterial()
     },
     getMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
-        // 全部的数据为fasle就是查看所有的图片
+        params: {
+          page: this.page.page,
+          per_page: this.page.pageSize,
 
+          collect: this.activeName === 'collect'
+        }
+        // 全部的数据为fasle就是查看所有的图片
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
-
   },
   created () {
     this.getMaterial()
   }
-
 }
 </script>
 
 <style lang="less" scoped>
-.material{
-  .card-list{
+.material {
+  .card-list {
     display: flex;
-    flex-wrap:wrap;
+    flex-wrap: wrap;
     justify-content: center;
-  .img-card{
-    width: 150px;
-    height: 150px;
-  margin: 20px;
-  position: relative;
-  img{
-     width: 100%;
+    .img-card {
+      width: 150px;
+      height: 150px;
+      margin: 20px;
+      position: relative;
+      img {
+        width: 100%;
         height: 100%;
-  }
-.operate{
-  position: absolute;
-  bottom: 0;
-  left:0;
-  width: 100%;
-  height: 30px;
-  background-color:#f4f5f6;
-  font-size: 18px;
+      }
+      .operate {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 30px;
+        background-color: #f4f5f6;
+        font-size: 18px;
+      }
+      .fen{
 
-}
-  }
-
+      }
+    }
   }
 }
 </style>
