93349d241fd17ffff5353bedeccf0f0bde5d135c
小武 <1376850803@qq.com>
2019-09-04 23:43:19 +0800

    内容列表-页面结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -16,8 +16,8 @@
     </el-radio-group>
         </el-form-item>
         <el-form-item label="频道列表:">
-            <el-select></el-select>
-        </el-form-item>
+          <el-select></el-select>
+      </el-form-item>
         <el-form-item label="时间选择:">
              <el-date-picker
       v-model="value1"
@@ -28,15 +28,87 @@
     </el-date-picker>
         </el-form-item>
     </el-form>
+    <!-- 内容页面结构 -->
+    <div class="total-info">共找到1213条复合条件的数据</div>
+    <div class='article-list'>
+         <!-- 循环项 -->
+         <div class="article-item" v-for="(item,index) in list" :key="index">
+            <!-- 左侧内容 -->
+            <div class="left">
+                <img src="../../assets/img/404.png" alt="">
+                <div class="info">
+                <span class="title">1111</span>
+                <el-tag style="width:60px">已发表</el-tag>
+                <span class="date">1111111</span>
+                </div>
+
+            </div>
+             <!-- 右侧内容 -->
+             <div class="right">
+                 <span><i class="el-icon-edit"></i>修改</span>
+                 <span><i class="el-icon-delete"></i>删除</span>
+             </div>
+         </div>
+
+    </div>
 </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: [1, 2, 3, 4, 5, 6]// 定义一个空数组
 
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.total-info{
+    border-bottom:1px dashed #ccc;
+    padding:15px 0;
+    color: #323745;
+  font-size: 14px;
+}
+.article-list{
+    .article-item{
+        display:flex;
+        justify-content: space-between;
+        padding: 15px 0;
+         border-bottom: 1px solid #f2f3f5;
+    }
+    .left{
+        display: flex;
+        img{
+            width: 180px;
+            height:100px;
+            border-radius:4px;
+        }
+        .info{
+            height: 100px;
+            display: flex;
+            flex-direction: column;
+            margin-left: 10px;
+            justify-content: space-around;
+            .title{
+                color: #333;
+                font-size: 14px;
+            }
+            .date{
+                color: #999;
+                font-size: 12px;
+            }
+        }
+    }
+    .right{
+        font-size: 12px;
+        span{
+            margin-right:8px;
+           cursor: pointer;
+            color:#333;
+        }
+    }
+}
 </style>
