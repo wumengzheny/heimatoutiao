3e42dc59cc65ead6f2d8bb50a20a5f303853fe15
小武 <1376850803@qq.com>
2019-09-05 19:04:05 +0800

    内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -7,7 +7,7 @@
     <el-form style="margin-left:40px">
         <el-form-item label="文章状态:">
 <!-- 文章状态，0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部 -->
-        <el-radio-group>
+        <el-radio-group @change="changeCondition" v-model="searchForm.status">
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
           <el-radio :label="1">待审核</el-radio>
@@ -16,18 +16,24 @@
          </el-radio-group>
         </el-form-item>
         <el-form-item label="频道列表:">
-          <el-select></el-select>
+          <el-select @change="changeCondition" v-model="searchForm.channel_id">
+              <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id">
+
+              </el-option>
+          </el-select>
       </el-form-item>
         <el-form-item label="时间选择:">
              <el-date-picker
-      v-model="value1"
+             @change="changeCondition"
+      v-model="searchForm.dateRange"
       type="daterange"
-      range-separator="至"
+      value-format="yyyy-MM-dd"
       start-placeholder="开始日期"
       end-placeholder="结束日期">
     </el-date-picker>
         </el-form-item>
-    </el-form>
+
+    </el-form>  {{searchForm}}
     <!-- 内容页面结构 -->
     <div class="total-info">共找到1213条复合条件的数据</div>
     <div class='article-list'>
@@ -60,22 +66,49 @@ export default {
     return {
       list: [], // 定义一个空数组
       defaultImg: require('../../assets/img/404.png'), // base64字符串
-      atatus: 5 // 状态
+      searchForm: {
+        status: 5, // 状态
+        channel_id: null,
+        dateRange: []// 数组[开始时间，结束时间]
+      },
+      channels: [] // 列表频道数据
 
     }
   },
-  // 查询文章的列表内容
+
   methods: {
-    getArticles () {
+    // 根据条件查询数据
+    changeCondition () {
+      let params = {
+        status: this.searchForm.status === 5 ? null : this.searchForm.status,
+        channel_id: this.searchForm.channel_id,
+        begin_pubdate: this.searchForm.dateRange.length > 0 ? this.searchForm.dateRange[0] : null,
+        end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null
+      }
+      this.getArticles(params)
+    },
+    // 查询文章的列表内容
+    getArticles (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(result => {
         this.list = result.data.results
       })
+    },
+    // 获取频道数据
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
     }
+
   },
   created () {
     this.getArticles()
+    this.getChannels()
   },
   filters: {
     statusText (value) {
