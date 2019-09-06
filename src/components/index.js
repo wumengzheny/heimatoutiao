e00d433a92c697512bb6466ef1585a1f0566df66
小武 <1376850803@qq.com>
2019-09-06 20:08:02 +0800

    发布文章-注册富文本编辑器及应用

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,17 @@ import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
 import breadCrumb from './common/bread-crumb'
 
+// 富文本编辑器
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 export default {
   install: function (Vue) {
     Vue.component('layout-aside', layoutAside)/// / 注册 左侧导航组件
     Vue.component('layout-header', layoutHeader)// 注册头部组件
     Vue.component('bread-crumb', breadCrumb) // 注册面包屑组件
+    Vue.component('quill-editor', quillEditor)// 注册富文本编辑器
   }
 }
